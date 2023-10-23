43f00bc51f9818babdfcc9e1bdaf43bb41e16ef3
Yashdeep Tandon <yashdeeptandon007@gmail.com>
2023-10-23 23:30:20 +0530

    section-3 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
+import reactImg from "./assets/react-core-concepts.png";
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {reactDescriptions[genRandomInt(2)]} React concepts you will need for
+        almost any app you are going to build!
+      </p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
