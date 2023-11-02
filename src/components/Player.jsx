dab830ec7a21cf86419b1d653b26bd82463d4f85
Yashdeep Tandon <yashdeeptandon007@gmail.com>
2023-11-02 17:43:08 +0530

    TicTacToe in progress

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,24 +1,31 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing((editing) => !isEditing);
+    setIsEditing((editing) => !isEditing); //! use this method for changing the value of variable instead of just using !isEditing because this will schedule react to change while function instantly changes the value
   }
 
-    let playerName = <span className="player-name">{name}</span>;
-    let btnCaption = "Edit";
+  function handleChange(event) {
+    console.log(event);
+    setPlayerName(event.target.value);
+  }
+  let editablePlayername = <span className="player-name">{playerName}</span>;
+  let btnCaption = "Edit";
 
   if (isEditing) {
-      playerName = <input type="text" required value={name}/>;
-      btnCaption = "Save";
+    editablePlayername = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+    btnCaption = "Save";
   }
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayername}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{btnCaption}</button>
